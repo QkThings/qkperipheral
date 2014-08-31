f2649437b9660e440f1cc8c2ac5d52c4815ca054
Mario Ribeiro <mario.ribas@gmail.com>
2014-08-31 03:04:59 +0100

    update

--- include/qk_peripheral.h
+++ include/qk_peripheral.h
@@ -4,6 +4,8 @@
 /**
 @mainpage QkPeripheral
 
+Configuring microcontroller's peripherals through registers is always a time consuming task because it requires to understand how a given peripheral works by reading the microcontroller's datasheet and writing low level code to make it work as desired, but it's also a critical aspect of any microcontroller based embedded system. Even though many microcontrollers, from different manufacturers, have peripherals with identical functionality they are set up quite differently. This peripheral library is an abstraction layer to those common peripherals. It provides generic and simple functions that implement the most essential function of a peripheral, greatly increasing code portability. Its use is, however, optional.
+
 */
 
 /** \addtogroup QkPeripheral
