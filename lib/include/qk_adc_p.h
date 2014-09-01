5e9cb108a4a23b7605604e8d32cbf51629d84117
Mario Ribeiro <mario.ribas@gmail.com>
2014-09-01 16:17:41 +0100

    LICENSE and README added

--- lib/include/qk_adc_p.h
+++ lib/include/qk_adc_p.h
@@ -1,5 +1,21 @@
-#ifndef QK_ADC_P_H
-#define QK_ADC_P_H
+/*
+ * QkThings LICENSE
+ * The open source framework and modular platform for smart devices.
+ * Copyright (C) 2014 <http://qkthings.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 void qk_adc_hw_startup();
 void qk_adc_hw_update();
